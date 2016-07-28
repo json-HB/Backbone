2f6eb1a18a7e3296db89e9fb437d34896d6f910f
json-HB <624172381@qq.com>
2016-07-29 01:36:44 +0800

    Create skin.js

---
+++ js/tools/modules/collection/model/skin.js
@@ -0,0 +1,11 @@
+// skin模块Model层
+define(function(){
+	var skinModel=Backbone.Model.extend({
+		initialize:function(){
+			this.on("add",function(model){
+				
+			})
+		}
+	})
+	return skinModel;
+})
