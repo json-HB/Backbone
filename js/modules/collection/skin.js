a79dfe54958ed9a94b2ee6cf8354868a0563a251
json-HB <624172381@qq.com>
2016-07-29 10:47:14 +0800

    Update skin.js

--- js/modules/collection/skin.js
+++ js/modules/collection/skin.js
@@ -1,35 +1,25 @@
-
-// img的集合
-define(function(require,exports,modules){
-	// 引用img模块
-	var imgModel=require("modules/model/img");
-
-	var imgCollection=Backbone.Collection.extend({
-		model:imgModel,
-		imageId:0,
-		// 请求img数据
-		feachData:function(callback,data){
-			var that=this;
-			// 发送ajax请求
-			$.get("data/imageList.json",function(res){
-				//console.log(res)
-				if(res.errno===0){
-						// 打乱图片排序
-						res.data.sort(function(){
-						return Math.random()-0.5;
-					})
-						// 给每个图片加上id
-					res.data.map(function(value){
-						value.id=that.imageId++;
-					})
-					//console.log(res.data)
-					that.add(res.data);
-					// 回调函数
-					callback&&callback();
-					}			
+define(function(require){
+	// 加载skin模块
+	var skinModel=require("modules/model/skin");
+	var skinCollect=Backbone.Collection.extend({
+		model:skinModel,
+		// 构造函数,只有在new的时候执行一次
+		initialize:function(){
+			//...
+		},
+		// 请求skin数据函数
+		feachData:function(callback){
+			var This=this;
+			// 发送get请求
+			$.get("data/imgSkin.json",function(res){
+				if(res.errno==0){
+					This.add(res.data);
+				}
 			})
-
+			// 回调函数
+			callback&&callback()
 		}
 	})
-	return imgCollection;
+	// 返回接口
+	return skinCollect;
 })
